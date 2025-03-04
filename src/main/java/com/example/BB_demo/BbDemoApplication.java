d0ea3a862116e344bf81ba94ceb9b3f7eb213ce7
vaishnavi08072000 <vnkathwate@gmail.com>
2025-03-04 14:05:14 +0530

    java-app

---
+++ src/main/java/com/example/BB_demo/BbDemoApplication.java
@@ -0,0 +1,15 @@
+package com.example.BB_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbDemoApplication.class, args);
+	}
+
+}
+
+
---
+++ src/test/java/com/example/BB_demo/BbDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BB_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
